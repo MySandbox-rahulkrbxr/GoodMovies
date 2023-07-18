658e5798c8323c4d65a9f167f4894ee8b8a77b9d
rahulkrbxr <61363513+rahulkrbxr@users.noreply.github.com>
2023-07-18 23:36:28 +0530

    Initial commit. Communication between 3 microservices using RestTemplate (& WebClient). Service Discovery using Eureka.

---
+++ ratings-data-service/src/main/java/com/nova/ratingsdataservice/Intro.java
@@ -0,0 +1,12 @@
+package com.nova.ratingsdataservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Intro {
+    @GetMapping("/")
+    public String greeting() {
+        return "ratings-data-service";
+    }
+}
